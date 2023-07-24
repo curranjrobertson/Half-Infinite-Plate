025d7058cffe1e4082ccccc629d391703b4bee85
curranjrobertson <curran.robertson@mines.sdsmt.edu>
2023-07-24 10:46:17 -0600

    final update

--- Area_plot.m
+++ Area_plot.m
@@ -1,14 +1,14 @@
 clear all; clc; close all
 
-alpha = [-90:1:90];
+alpha = [-90:1:90]; % angle alpha
 
-dx = 0.01
-dy = 0.6784*dx
+dx = 0.01 % arbitrary side length
+dy = 0.6784*dx % vertical side length
 
-A = dx.*(dy+(dx/2).*tand(alpha));
+A = dx.*(dy+(dx/2).*tand(alpha)); % Equation for area
 
-plot(alpha,A);
+plot(alpha,A); % plot the area wrt alpha
 legend
 
-
-fzero(@(a) dx*(dy+(dx)*tand(a)),0)
+fzero(@(a) dx*(dy+(dx)*tand(a)),0) % find where the area is zero
+C = dy/dx % print the coefficient
--- half_infinite_plate.m
+++ half_infinite_plate.m
@@ -13,10 +13,10 @@ for dx = f:f:1
     dy = dx;
         
     while j <= (180)/1
-alpha(j)
+          alpha(j)
           A(i,j) = dx.*(dy-(dx/2).*tand(alpha(j)));
-%           dA(i,j) = dx.*(dy+(dx/2).*secd(alpha(j)).^2);
-%           ddA(i,j) = dx.*(dy+(dx).*tand(alpha(j)).*secd(alpha(j)).^2);
+          dA(i,j) = dx.*(dy+(dx/2).*secd(alpha(j)).^2);
+          ddA(i,j) = dx.*(dy+(dx).*tand(alpha(j)).*secd(alpha(j)).^2);
           j = j+1;
     end
     
@@ -31,50 +31,23 @@ title('Area as a function of dx for different angles alpha')
 xlabel('dx')
 ylabel('Area')
 
-% figure
-% plot(x,dA)
-% hold on 
-% legend
-% title('Derivative of the area (wrt alpha)as a function of dx')
-% xlabel('dx')
-% ylabel('dA')
+figure
+plot(x,dA)
+hold on 
+%legend
+title('Derivative of the area (wrt alpha)as a function of dx')
+xlabel('dx')
+ylabel('dA')
 
-% figure
-% plot(x,ddA)
-% hold on 
-% legend
-% title('Second Derivative of the area (wrt alpha)as a function of dx')
-% xlabel('dx')
-% ylabel('ddA')
-
-%%
-% Plot Area for one angle with decreasing size
-clear all; clc; close all
-
-alpha = -63.5
+figure
+plot(x,ddA)
+hold on 
+%legend
+title('Second Derivative of the area (wrt alpha)as a function of dx')
+xlabel('dx')
+ylabel('ddA')
 
 
-f = 0.01;
-
-i = 1;
-j = 1;
-
-for dx = 1:-f:f
-    x(i) = dx;
-    dy = dx;
-    A(i) = dx.*(dy+(dx/2).*tand(alpha))
-    %dA(i) = dx.*(dy+(dx/2).*secd(alpha).^2);
-    i = i+1;
-end
-
-plot(x,A)
-hold on
-legend
-
-% figure
-% plot(x,dA)
-% hold on 
-% legend
 
 
 
